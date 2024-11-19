20cff050c88eb4cfbd9c57b2522f96daf7a72c87
fatahpratam <fatahpratamasolutions@gmail.com>
2024-11-19 13:13:47 +0700

    Add all starter files to Git

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [counter, setCounter] = useState(15);
+  // let counter = 15;
+
+  const addValue = () => {
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1);
+  }
+
+  return (
+    <>
+      <h1>React Course With Hitesh {counter}</h1>
+      <h2>Counter value: {counter}</h2>
+      <button
+        onClick={addValue}
+      >Add value</button>{' '}
+      <button
+        onClick={removeValue}
+      >Remove value</button>
+      <p>Footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
